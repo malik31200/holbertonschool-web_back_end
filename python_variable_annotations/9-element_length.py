004b485648f7593c546e6fc84bd49db2969ac4cc
malik31200 <yka-vtc@outlook.fr>
2025-11-24 11:24:10 +0100

    return values with the appropriate types

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module annotate a function's paramater.
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    return values with the appropriate types
+    """
+    return [(i, len(i)) for i in lst]
