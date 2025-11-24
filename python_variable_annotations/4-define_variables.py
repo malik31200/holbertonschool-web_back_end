9e5d94d8e1ca218a50dd3275f0c743f05f5b1c74
malik31200 <yka-vtc@outlook.fr>
2025-11-24 10:33:57 +0100

    Defines and annotates the variables with their values

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+the module defines and annotates 
+the variables with their specifed values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
