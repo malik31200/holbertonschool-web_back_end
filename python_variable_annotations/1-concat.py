d8189a0b4376932b1e6b130cf7bca88ce996d750
malik31200 <yka-vtc@outlook.fr>
2025-11-24 10:09:39 +0100

    return a concataned string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+The module contains a function that takes
+two strings as arguments and
+return a concatenated string 
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Return a concataned string.
+    """
+    return str1 + str2
