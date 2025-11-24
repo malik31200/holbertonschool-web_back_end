b9a3846e4b64a32fdfa421f53b9ddd877d8b3722
malik31200 <yka-vtc@outlook.fr>
2025-11-24 10:19:16 +0100

    return the floor of the n

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function which takes a float 'n'
+as argument and returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of the n.
+    """
+    return math.floor(n)
