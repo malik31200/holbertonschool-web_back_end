b8e3870249c69e8bcd828bf1904b37255c560a89
malik31200 <yka-vtc@outlook.fr>
2025-11-24 10:27:08 +0100

    return the string representation of the float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module contains a function that takes a float n as argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of the float.
+    """
+    s = str(n)
+    return s
