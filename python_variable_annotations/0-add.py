00a115351255d6a28e063d1f6ea38099e538d228
malik31200 <yka-vtc@outlook.fr>
2025-11-24 09:04:57 +0100

    add function that return the sum as float

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module content a function that add two floats
+and return their sum
+"""
+
+
+def add(a: float, b: float):
+    """
+    Return the sum of a and b as float.
+    """
+    return a + b
