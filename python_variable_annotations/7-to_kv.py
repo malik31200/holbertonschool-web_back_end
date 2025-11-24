b6c72edbe0cdea99ccd2d947b0f7dbe2f8f918b2
malik31200 <yka-vtc@outlook.fr>
2025-11-24 11:56:10 +0100

    fix: add Union, Tuple  typing

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,9 +3,10 @@
 Module contains a function that takes a string and
 an int OR float as arguments and returns a tuple.
 """
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Return a tuple with the first element of the tuple is the string.
     The second element is the square of the int/float v.
