507e7085a2ba3c37166e293325300538042f7283
malik31200 <yka-vtc@outlook.fr>
2025-11-24 10:44:48 +0100

    Return the sum of a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module contains a function which takes a list
+of floats as argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    return the sum of a list.
+    """
+    return sum(input_list)
