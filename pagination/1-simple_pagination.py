69c986c2fb7e2a3bd3feaf3c1b72400ce283c721
malik31200 <yka-vtc@outlook.fr>
2025-12-01 11:45:16 +0100

    returns the appropriate page of the dataset.

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Pagination helper module.
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Return a tuple containing a start and an end index for pagination.
+
+    Args:
+        page (int): The current page number (1-indexed)
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple [int, int]: A tuple (start_index, end_index).
+    """
+    start_index: int = (page - 1) * page_size
+    end_index: int = page * page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns the appropriate page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.dataset()
+
+        start, end = index_range(page, page_size)
+
+        if start >= len(data):
+            return []
+
+        return data[start:end]
