640adf18898f1fc3bbd88486014109d4f7da4ebc
malik31200 <yka-vtc@outlook.fr>
2025-12-02 10:37:45 +0100

    change docstrings

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 """
-Function that changes all topics of a school document based on the name.
+Update all topics of a school document based on the name.
 """
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics of a school
+    Args:
+        mongo_collection: pymongo collection object
+        name: string, school name to update
+        topics: list of strings, topics to set
+    """
     mongo_collection.update_many(
         {"name": name},
-        { "$set": {"topics": topics}}
+        {"$set": {"topics": topics}}
     )
