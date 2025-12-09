6fb6c0c28d66b2083d50680d5175f57101ba1e7e
malik31200 <yka-vtc@outlook.fr>
2025-12-09 15:04:54 +0100

    fix code

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,24 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-
     this._sqft = sqft;
 
-    if (
-      this.evacuationWarningMessage === undefined
-      || this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
-    ) {
+    if (this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
