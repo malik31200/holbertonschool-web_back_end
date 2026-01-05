e1bed5ae7cfefaf1490953bee027f267b35b2d5a
malik31200 <yka-vtc@outlook.fr>
2026-01-05 11:23:03 +0100

    function to read CSV synchronously, count students per field and list names

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs'); // Import the file system module to read files
+
+function countStudents(path) {
+  let data = ''; // store file contents
+
+  // read the file synchro
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = data.split('\n').filter((line) => line.trim() !== '');// ignore empty lines
+  const students = lines.slice(1); // ignore the header.
+
+  console.log(`Number of students: ${students.length}`);
+
+  const fields = {}; // Creating an object to store students by field
+  students.forEach((line) => {
+    const [firstname, , , field] = line.split(','); // retrieve the firstname name and field
+    if (!fields[field]) fields[field] = []; // If the field does not exist, create an array
+    fields[field].push(firstname); // add the firstname at this field
+  });
+
+  for (const field in fields) {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
