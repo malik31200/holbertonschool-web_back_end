d8d386b2c9d1d3baf9b27de73acbdeb84bd56d9d
malik31200 <yka-vtc@outlook.fr>
2026-01-05 19:55:59 +0100

    Express server with / and /students endpoints

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.type('text');
+
+  countStudents(database)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
