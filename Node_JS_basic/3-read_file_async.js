4f77780c34741f1dbee3d9a81fd4c7e98cec34cf
malik31200 <yka-vtc@outlook.fr>
2026-01-05 13:36:07 +0100

    implement countStudents with asynchronous file reading

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,31 @@
+const { count } = require('console');
+
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  let data = '';
+
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+      students.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      });
+
+       Object.keys(fields).forEach((field) => {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      });
+    })
+    .catch(() =>{
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
