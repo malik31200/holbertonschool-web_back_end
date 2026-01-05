1235af21a3b2bef1950c1fc14609e9c76f812589
malik31200 <yka-vtc@outlook.fr>
2026-01-05 09:00:13 +0100

    delete comments

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 process.stdin.on('end', () => {
