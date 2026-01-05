7302b089e4fe1eecf16bb22e8134c7c783d7a2e0
malik31200 <yka-vtc@outlook.fr>
2026-01-05 08:52:38 +0100

    Fix: replace console.log by process.stdout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@ process.stdin.on('data', (input) => {
   // process.stdin to read the user input
   const name = input.toString().trim();
   // input is a buffer, convert it to a string and remove the line break.
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 process.stdin.on('end', () => { // ctrl + D
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
