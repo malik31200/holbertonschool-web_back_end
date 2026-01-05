2b077ea7a53bdcf2a71835060b345c0c1407896f
malik31200 <yka-vtc@outlook.fr>
2026-01-05 08:54:20 +0100

    delete comments

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
-  // process.stdin to read the user input
   const name = input.toString().trim();
-  // input is a buffer, convert it to a string and remove the line break.
   process.stdout.write(`Your name is: ${name}\n`);
 });
-process.stdin.on('end', () => { // ctrl + D
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
