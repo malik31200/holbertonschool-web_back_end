39184089e938a96fcc808f9ba8f229184dcfe636
malik31200 <yka-vtc@outlook.fr>
2026-06-01 11:52:14 +0200

    add Readme file + module cache

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A basic caching system with no limit."""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
