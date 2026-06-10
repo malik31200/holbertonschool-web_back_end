1274bb9b6e58a38d6161eef04361c24d0b6eee79
malik31200 <yka-vtc@outlook.fr>
2026-06-10 15:55:23 +0200

    function that determines if authentication is required

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,24 @@ from typing import List, TypeVar
 class Auth():
     """ Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Returns False for now """
-        return False
+        """ Determine if authentication is required """
+        if path is None:
+            return True
 
-    def authorization_header(self, request=None) -> str:
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self,
+                             request=None) -> str:
         """  Returns None for now """
         return None
 
