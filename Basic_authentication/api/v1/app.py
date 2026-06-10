9782cf0bbb8161ae0a898274b35523abe4cdb4c3
malik31200 <yka-vtc@outlook.fr>
2026-06-10 16:39:14 +0200

    add request validation

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,6 +13,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Auth initialization
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +39,32 @@ def forbidden(error) -> str:
     """ Handle 403 error
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+# Before request filter
+@app.before_request
+def before_request():
+    """ Filter all incoming requests before routing """
+    if auth is None:
+        return None
+
+    excluded_paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/"
+    ]
+
+    # If route is public -> allow request
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    # If no Authorization header -> 401
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    # If user is not valid -> 403
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
