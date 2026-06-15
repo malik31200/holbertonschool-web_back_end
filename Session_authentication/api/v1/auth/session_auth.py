1ac05df208d75cf8f4501fccbbb7a614fa1c5ea6
malik31200 <yka-vtc@outlook.fr>
2026-06-15 13:48:39 +0200

    add session auth to identify the user

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,11 +4,30 @@ SessionAuth module
 """
 
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     SessionAuth class
-    INherits from Auth but does nothing yet
+    that stores user session in memory
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID dor a user_id
+        """
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        # Generate a ID session unique
+        session_id = str(uuid.uuid4())
+
+        # Store in dictionary
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
