4aa3249641177367811f3f756fb350cbc930d172
malik31200 <yka-vtc@outlook.fr>
2026-06-16 11:00:17 +0200

    add logout

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -68,3 +68,22 @@ class SessionAuth(Auth):
         user = User.get(user_id)
 
         return user
+
+    def destroy_session(self, request=None):
+        """ Logout user by deleting session """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if user_id is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+
+        return True
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Session authentication views"""
 
-from flask import request, jsonify, make_response
+from flask import request, jsonify, abort
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
@@ -53,3 +53,18 @@ def login():
     )
 
     return response
+
+
+@app_views.route(
+    '/auth_session/logout',
+    methods=['DELETE']
+    strict_slashes=False
+)
+def logout():
+    """ Logout user (destroy session) """
+
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+
+    return jsonify({}), 200
