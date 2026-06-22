35f0f8c5654feff51110c2879b18503620f4bf23
malik31200 <yka-vtc@outlook.fr>
2026-06-22 13:33:55 +0200

    fix: add type to email

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -28,7 +28,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Adds  a user to database and
         return  the User object
