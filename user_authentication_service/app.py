62a5bc77c8fff574875b8a60073a0f3045d73214
malik31200 <yka-vtc@outlook.fr>
2026-06-23 11:19:57 +0200

    log in route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """A basic flask app
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -37,6 +37,27 @@ def users():
         return jsonify({
             "message": "email already registered"
         }), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """ Login user and create session"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = make_response(jsonify({
+        "email": email,
+        "message": "logged in"
+    }))
+
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
