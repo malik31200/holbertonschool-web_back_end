8b0f3d42edc2451c762641c3b2db55e0391df1bb
malik31200 <yka-vtc@outlook.fr>
2026-06-23 11:27:56 +0200

    Returns user based on session_id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,3 +85,17 @@ class Auth:
 
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str):
+        """
+        Returns user based on session_id
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+
+        except NoResultFound:
+            return None
