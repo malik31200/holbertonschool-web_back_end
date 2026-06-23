cd769fa26a1fc2110e45410d59dc717ff33ff8ed
malik31200 <yka-vtc@outlook.fr>
2026-06-23 12:49:29 +0200

    reset password method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -103,3 +103,22 @@ class Auth:
     def destroy_session(self, user_id: str):
         """ Destroy user session (logout) """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generate reste password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+
+        self._db.update_user(
+            user.id,
+            reset_token=reset_token
+        )
+
+        return reset_token
