3616ff1a50ee2f8e5dc48fa2da332f5acb94bc8b
malik31200 <yka-vtc@outlook.fr>
2026-07-06 16:38:15 +0200

    Test that keyError is raised when key is not found         and that the exception message is correct.

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -23,3 +23,20 @@ class TestAccessNestedMap(unittest.TestCase):
         the expected value
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """
+        Test that keyError is raised when key is not found
+        and that the exception message is correct.
+        """
+
+        # Check if the function raised a exception key Error
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Check that the key in the exception is correct
+        self.assertEqual(context.exception.args[0], expected)
