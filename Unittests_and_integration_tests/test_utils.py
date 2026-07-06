32d8e5b6f50d7ecb7e9aace5ed99e6c461d83664
malik31200 <yka-vtc@outlook.fr>
2026-07-06 17:27:04 +0200

    Test that get_json returns expected JSON

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,8 @@ Unit tests for utils.access_nested_map function.
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -40,3 +41,26 @@ class TestAccessNestedMap(unittest.TestCase):
 
         # Check that the key in the exception is correct
         self.assertEqual(context.exception.args[0], expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test for get_json function.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test that get_json returns expected JSON
+        and that requests.get is called correctly.
+        """
+        mock_get.return_value.json.return_value = test_payload
+
+        result = get_json(test_url)
+
+        self.assertEqual(result, test_payload)
+
+        mock_get.assert_called_once_with(test_url)
