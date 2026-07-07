dcfdfb5ba115806e2c2de9aff755a90311401179
malik31200 <yka-vtc@outlook.fr>
2026-07-07 11:47:19 +0200

    test that has_license returns the expected value

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -99,3 +99,24 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             "https://api.github.com/orgs/google/repos"
             )
+
+    @parameterized.expand([
+        (
+            {"license": {"key": "my_license"}},
+            "my_license",
+            True
+        ),
+        (
+            {"license": {"key": "other_license"}},
+            "my_license",
+            False
+        ),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test for GithubOrgClient.has_license
+        Check that has_license returns the expected value.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+
+        self.assertEqual(result, expected)
