883d21f533fd15dba8dd1676ae64915573af2731
malik31200 <yka-vtc@outlook.fr>
2026-07-08 10:22:54 +0200

    add babel flask with default languages

---
+++ i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel configuration.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration class for Babel.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Display the home page
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
