4c30bc0ca90177b557191651eb350339816a2d43
malik31200 <yka-vtc@outlook.fr>
2026-07-14 10:08:47 +0200

    get data in desired format

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Redis cache
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache():
@@ -25,3 +25,29 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """
+        Get data in Redis  and optionnaly convert it
+        """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn is not None:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Get string value from Redis
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: int) -> int:
+        """
+        Get integer value from Redis
+        """
+        return self.get(key, fn=int)
