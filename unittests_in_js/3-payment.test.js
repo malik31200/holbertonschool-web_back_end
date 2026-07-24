e8c100f30a8b0e1056abe6cdeeb17f60d1597413
malik31200 <yka-vtc@outlook.fr>
2026-07-24 08:50:03 +0200

    add spies

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with SUM, 100 and 20', function () {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+
+    spy.restore();
+  });
+});
