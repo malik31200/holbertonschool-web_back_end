9dd69cb0cc168133f7b708c7f85c949b689d633c
malik31200 <yka-vtc@outlook.fr>
2026-07-24 08:55:57 +0200

    Basic Integration testing

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', function () {
+
+  it('should return status code 200', function (done) {
+
+    request.get('http://localhost:7865/', (error, response) => {
+
+      expect(response.statusCode).to.equal(200);
+
+      done();
+
+    });
+
+  });
+
+
+  it('should return the correct message', function (done) {
+
+    request.get('http://localhost:7865/', (error, response, body) => {
+
+      expect(body).to.equal('Welcome to the payment system');
+
+      done();
+
+    });
+
+  });
+
+});
